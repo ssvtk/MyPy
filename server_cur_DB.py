66ebbfb65aa5ceaa5560be11cc2434a7ce24d1b9
Slava Tkachenko <svtkjs@gmail.com>
2019-07-13 09:58:59 +0700

    init commit one

---
+++ server_cur_DB.py
@@ -0,0 +1,34 @@
+import psycopg2
+import time as t
+
+con = psycopg2.connect(
+    host='localhost',
+    database='sample_db',
+    user='postgres',
+    password='111111',
+    port=5432
+
+)
+
+
+s = t.time()
+
+
+cur = con.cursor('sltk')
+e = (t.time() - s) * 1000
+
+print(f'Cursor established in {e}')
+
+s = t.time()
+cur.execute('SELECT * FROM employee')
+e = (t.time() - s) * 1000
+print(f'query all rows established in {e}')
+
+s = t.time()
+rows = cur.fetchmany(50)
+e = (t.time() - s) * 1000
+print(f'Fetching 50 rows {e}')
+
+
+cur.close()
+con.close()
